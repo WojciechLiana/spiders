feecd2cb3dd009f06c907a6c0700d1a20ac92354
wojtek <wojtek.liana@gmail.com>
2021-07-09 20:34:15 +0200

    environment

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import "../src/index.sass";
+import React from "react";
+
+export default function App() {
+  return <h1>Interia</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+      stats: "errors-only",
+      overlay: true,
+      contentBase: path.join(__dirname, 'dist'),
+      host: process.env.HOST,
+      port: process.env.PORT,
+      open: true,
+  }
+};
