2fd8fcb37851272bd0ec779d1143d061ba255fc3
wojtek <wojtek.liana@gmail.com>
2021-07-11 19:56:58 +0200

    cleaning

--- src/components/spider/Spider.js
+++ src/components/spider/Spider.js
@@ -8,22 +8,24 @@ export const Spider = React.memo(({spider, moveSpider, leaveSpider}) => {
 
   const defaultStyle = R.defaultTo(0);
 
+  const onMouseMoveHandler = (e) => {mouseDown ? moveSpider({x: e.clientX, y: e.clientY, spiderNumber: spider.spiderNumber}) : null};
+  const onTouchMoveHandler = (e) => {mouseDown ? moveSpider({x: e.touches[0].clientX, y: e.touches[0].clientY, spiderNumber: spider.spiderNumber}) : null};
+  const onTakeSpiderHandler = () => setMouseDown(true);
+  const onLeaveSpider = () => {
+    setMouseDown(false);
+    leaveSpider();
+  }
+
   return (
     <div
       className="spider"
       style={{left: defaultStyle(spider.x - 50), top: defaultStyle(spider.y - 50)}}
-      onMouseDown={()=>setMouseDown(true)}
-      onMouseUp={()=>{
-        setMouseDown(false);
-        leaveSpider();
-      }}
-      onTouchStart={()=>setMouseDown(true)}
-      onTouchEnd={()=>{
-        setMouseDown(false);
-        leaveSpider();
-      }}
-      onMouseMove={e => mouseDown ? moveSpider({x: e.clientX, y: e.clientY, spiderNumber: spider.spiderNumber}) : null}
-      onTouchMove={e => mouseDown ? moveSpider({x: e.touches[0].clientX, y: e.touches[0].clientY, spiderNumber: spider.spiderNumber}) : null}
+      onMouseDown={onTakeSpiderHandler}
+      onMouseUp={onLeaveSpider}
+      onTouchStart={onTakeSpiderHandler}
+      onTouchEnd={onLeaveSpider}
+      onMouseMove={onMouseMoveHandler}
+      onTouchMove={onTouchMoveHandler}
     />
   );
 });
--- src/components/winWindow/WinWindow.js
+++ src/components/winWindow/WinWindow.js
@@ -5,7 +5,7 @@ export default function WinWindow({nextLevelClick}) {
 
   return(
     <div className="win-window">
-      <span className="win-window-button" onClick={()=>nextLevelClick(true)}>
+      <span className="win-window-button" onClick={nextLevelClick.bind(null, true)}>
         <div className="win-window-label">Next Level</div>
         <div className="win-window-icon">&#10174;</div>
       </span>
